c174221a8024ad9255f42f1cd1b363e056a24897
Tim Pietrusky <timpietrusky@gmail.com>
2025-02-18 21:16:41 +0100

    feat: experimental banner animation

--- components/landing/experimental-banner.tsx
+++ components/landing/experimental-banner.tsx
@@ -7,14 +7,16 @@ interface ExperimentalBannerProps {
 export function ExperimentalBanner({ className }: ExperimentalBannerProps) {
 	return (
 		<div className={cn("w-full bg-black relative overflow-hidden flex flex-col py-2", className)}>
-			<div className="w-full h-4 bg-[repeating-linear-gradient(45deg,#000,#000_10px,#e41832_10px,#e41832_20px)]"></div>
+			{/* Top line pattern */}
+			<div className="w-full h-4 bg-[repeating-linear-gradient(45deg,#000,#000_10px,#e41832_10px,#e41832_20px)] animate-experimental-bg"></div>
 			<div className="py-4 text-center flex-1 flex flex-col justify-start">
 				<h2 className="text-[#e41832] text-3xl md:text-5xl font-black mb-2 tracking-tight">
 					EXPERIMENTAL
 				</h2>
 				<p className="text-lg">this is an alpha release, expect everything to be totally broken</p>
 			</div>
-			<div className="w-full h-4 bg-[repeating-linear-gradient(45deg,#000,#000_10px,#e41832_10px,#e41832_20px)]"></div>
+			{/* Bottom line pattern */}
+			<div className="w-full h-4 bg-[repeating-linear-gradient(45deg,#000,#000_10px,#e41832_10px,#e41832_20px)] animate-experimental-bg-reverse"></div>
 		</div>
 	);
 }
