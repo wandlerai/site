dbd36fc4f519276b60eccc66afdde8f38ff8369e
Tim Pietrusky <timpietrusky@gmail.com>
2025-02-19 21:15:56 +0100

    refactor: rotate-2 of the top line

--- components/landing/roadmap-section.tsx
+++ components/landing/roadmap-section.tsx
@@ -72,13 +72,14 @@ const roadmapItems: RoadmapItem[] = [
 
 export function RoadmapSection({ className }: RoadmapSectionProps) {
 	return (
-		<section className={cn("relative bg-black w-full overflow-x-hidden", className)}>
+		<section className={cn("relative bg-black w-full", className)}>
 			<div className="absolute inset-0 bg-black">
 				<div className="absolute inset-0 bg-noise"></div>
 			</div>
+			{/* Top line pattern */}
+			<div className="absolute -top-12 rotate-2 left-0 right-0 h-[30px] md:h-[45px] bg-[repeating-linear-gradient(90deg,theme(colors.primary.DEFAULT),theme(colors.primary.DEFAULT)_10px,transparent_10px,transparent_20px)] md:bg-[repeating-linear-gradient(90deg,theme(colors.primary.DEFAULT),theme(colors.primary.DEFAULT)_15px,transparent_15px,transparent_30px)] opacity-60"></div>
+
 			<div className="h-[15vh] md:h-[50vh] w-full relative z-10 flex items-end border-l-[50px] md:border-l-[300px] border-primary/40 border-dash-dense">
-				{/* Top line pattern */}
-				<div className="absolute top-0 left-2 md:left-4 right-0 h-[30px] md:h-[45px] bg-[repeating-linear-gradient(90deg,theme(colors.primary.DEFAULT),theme(colors.primary.DEFAULT)_10px,transparent_10px,transparent_20px)] md:bg-[repeating-linear-gradient(90deg,theme(colors.primary.DEFAULT),theme(colors.primary.DEFAULT)_15px,transparent_15px,transparent_30px)] opacity-60"></div>
 				<div className="w-full">
 					<h2 className="text-[12vw] md:text-[10vw] text-left font-black mb-0 text-primary tracking-tight uppercase relative container leading-[0.75] bg-transparent pl-4 md:pl-0">
 						roadmap
