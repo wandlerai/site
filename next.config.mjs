cc069d540f5f66bd05074dfbc2fb0a1e06fcaa11
Tim Pietrusky <tim.pietrusky@runpod.io>
2025-03-16 20:23:57 +0100

    fix: added custom webpack config to ignore onnxruntime-node

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,18 @@ const config = {
 		],
 	},
 	serverExternalPackages: ["@huggingface/transformers", "onnxruntime-node"],
+
+	// Override the default webpack configuration
+	webpack: config => {
+		// Ignore node-specific modules when bundling for the browser
+		// See https://webpack.js.org/configuration/resolve/#resolvealias
+		config.resolve.alias = {
+			...config.resolve.alias,
+			sharp$: false,
+			"onnxruntime-node$": false,
+		};
+		return config;
+	},
 };
 
 export default config;
