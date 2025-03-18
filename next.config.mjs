8b6363dfc4f893b6529cccea853de45719cb584f
Tim Pietrusky <tim.pietrusky@runpod.io>
2025-03-18 12:15:03 +0100

    fix: update to next 15.2.3 & simplified webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -15,29 +15,14 @@ const config = {
 	serverExternalPackages: ["onnxruntime-node", "@huggingface/transformers"],
 
 	// Override the default webpack configuration
-	webpack: (config, { isServer }) => {
+	webpack: config => {
 		// Ignore node-specific modules when bundling for the browser
 		// See https://webpack.js.org/configuration/resolve/#resolvealias
 		config.resolve.alias = {
 			...config.resolve.alias,
 			sharp$: false,
 			"onnxruntime-node$": false,
-			"onnxruntime-node/bin": false,
-			"onnxruntime-node/bin/napi-v3/linux": false,
-			"onnxruntime-node/bin/napi-v3/win32": false,
-			"onnxruntime-node/bin/napi-v3/darwin": false,
 		};
-
-		// Add specific handling for the server bundle
-		if (isServer) {
-			// Ensure onnxruntime-node is not included in the server bundle
-			config.externals = [
-				...(config.externals || []),
-				"onnxruntime-node",
-				"@huggingface/transformers",
-			];
-		}
-
 		return config;
 	},
 };
